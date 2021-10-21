d69704740ca79ccc684d103cb534fd7ba1645f7a
imanmaliki11 <imanmaliki11@gmail.com>
2021-10-21 09:40:18 +0700

    move hitting API to apiAll files

--- src/api/allAPI.js
+++ src/api/allAPI.js
@@ -1,21 +1,20 @@
 import axios from "axios";
 
-const News = () => {
+export const News = key => {
+    const url = 'https://free-news.p.rapidapi.com/v1/search'
     return axios({
         method: 'GET',
-        url: 'https://free-news.p.rapidapi.com/v1/search',
-        params: { q: 'Elon Musk', lang: 'en' },
+        url: url,
+        params: { q: key, lang: 'en' },
         headers: {
             'x-rapidapi-host': 'free-news.p.rapidapi.com',
             'x-rapidapi-key': 'c99cc0c1e2msh7489ad3aaec92b7p1ab10ejsn69541b1f84d0'
         }
     })
         .then((res) => {
-            console.log(res);
-            return [
-                { id: 1, nama: "Iman Maliki" }
-            ]
+            return res.data
+        })
+        .catch((e) => {
+            return e
         });
 }
-
-export default News;
